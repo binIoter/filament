dc1e39034f4f4d4d0ab9b0094f05ab5de3ff9cd9
Philip Rideout <philiprideout@gmail.com>
2020-08-18 11:35:51 -0700

    Fix VSCode warning.

--- filament/backend/src/vulkan/VulkanContext.cpp
+++ filament/backend/src/vulkan/VulkanContext.cpp
@@ -237,6 +237,7 @@ void createLogicalDevice(VulkanContext& context) {
         .physicalDevice = context.physicalDevice,
         .device = context.device,
         .pVulkanFunctions = &funcs,
+        .pRecordSettings = nullptr,
         .instance = context.instance
     };
     vmaCreateAllocator(&allocatorInfo, &context.allocator);
